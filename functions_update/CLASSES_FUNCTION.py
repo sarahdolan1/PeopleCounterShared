0194cefcefb8b983bf09a40fe308a3d9e0d5de23
sarahdolan1 <66939149+sarahdolan1@users.noreply.github.com>
2020-07-22 11:16:58 +0100

    update on functions

---
+++ functions_update/CLASSES_FUNCTION.py
@@ -0,0 +1,20 @@
+#these function will get detections and be a centroid tracker
+#def trackPeople():
+ #   CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+  #             "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+   #            "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+    #           "sofa", "train", "tvmonitor"]
+
+    #return CLASSES
+
+#def getDetections():
+#use blob to get the dections to detect the people
+#return
+
+#def centroidTracker():
+#find centroids of poeple to help track people
+#return
+
+#def checkBoundary():
+#used in future final code; irrelevant for now
+#return
